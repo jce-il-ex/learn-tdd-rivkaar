19ef495ca72d23fa822a050953282454a4ae7e05
Tmura <Tmura>
2017-01-10 18:49:08 +0200

    red: a failing test

--- test/test.js
+++ test/test.js
@@ -1,2 +1,14 @@
 
 var C = require('../cash.js');  // our module
+
+var assert = require("assert"); // core module
+var C = require('../cash.js');  // our module
+
+describe('Cash Register', function(){
+  describe('Module C', function(){
+    it('should have a getChange Method', function(){
+      assert.equal(typeof C, 'object');
+      assert.equal(typeof C.getChange, 'function');
+    })
+  })
+});
