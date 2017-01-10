cbeca3855533fd67b0aea50ec05c55eb1e4cb772
Tmura <Tmura>
2017-01-10 19:03:37 +0200

    blue

--- test/test.js
+++ test/test.js
@@ -54,3 +54,5 @@ C.getChange = function (totalPayable, cashPaid) {
 it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
     assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
 });
+
+
